7aad860ccd92d51a1974bdd1ae5ae3c56f8adb70
Samuel Alves <samuel.br.igt@gmail.com>
2018-04-27 20:14:36 -0300

    *Adicionado spwan em ponto aleatório e vôo em direção aleatória.

---
+++ Assets/Script/DuckController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckController : MonoBehaviour {
+	private Vector3 dir;
+	private Vector3 angle;
+
+	public float minAngleX;
+	public float maxAngleX;
+	public float minAngleY;
+	public float maxAngleY;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		angle = lookAngle ();
+		transform.LookAt (angle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += (transform.forward * speed) * Time.deltaTime;
+		//transform.position += (Vector3.forward * speed) * Time.deltaTime;
+		
+	}
+	private Vector3 lookAngle(){
+		float x = Random.Range (minAngleX, maxAngleX);
+		float y = Random.Range (minAngleY, maxAngleY);
+		return new Vector3 (x, y, 0);
+	}
+}
---
+++ Assets/Script/SpawnController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour {
+	public GameObject duck;
+	public Transform[] waypoints;
+	public int numberOfSpawnElements = 1;
+	public int spawnInterval = 5;
+
+	private int nextTime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time >= nextTime){
+			spawnDuck ();
+			nextTime = Mathf.FloorToInt (Time.time) + spawnInterval;
+		}
+
+	}
+
+	public void spawnDuck(){
+		GameObject newDuck = Instantiate (duck) as GameObject;
+		Transform[] randomPoints = getRandomPoint ();
+		for (int i = 0; i < randomPoints.Length; i++) {
+			newDuck.transform.position = randomPoints[i].position	;
+		}
+		Debug.Log(getRandomPoint().Length);
+	}
+
+	private Transform[] getRandomPoint(){
+		int min = 0;
+		int max = waypoints.Length;
+		Transform[] randomPoints = new Transform[numberOfSpawnElements];
+		for(int i = 0; i < numberOfSpawnElements; i++){
+			randomPoints[i] = waypoints[Random.Range(min, max)];
+		}
+		return randomPoints;
+	}
+}

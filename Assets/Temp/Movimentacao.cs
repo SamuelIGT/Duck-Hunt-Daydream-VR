c604b06717e09208885b9d7d7c01d736a0ec0adf
Isaac Bruno Brandão Maia Lima <isaac-pj@hotmail.com>
2018-05-15 21:35:15 -0300

    alguns modelos

---
+++ Assets/Temp/Movimentacao.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimentacao : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			this.gameObject.transform.Translate (0,0,1*Time.deltaTime);		
+		}
+		if(Input.GetKey(KeyCode.UpArrow)){
+			this.gameObject.transform.Translate (0,0,1*Time.deltaTime);
+		}else if (Input.GetKey(KeyCode.DownArrow)){
+			this.gameObject.transform.Translate (0,0,-1*Time.deltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			this.gameObject.transform.Rotate (0,-1,0);
+		}
+		else if (Input.GetKey(KeyCode.RightArrow)){
+			this.gameObject.transform.Rotate (0,1,0);
+		}
+
+	}
+} 	
+
